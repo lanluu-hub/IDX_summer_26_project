6b6a85cc7e8ef568cb96656419812d82fe3bafdb
Lan Quy Luu <luuquylan.keil@gmail.com>
2026-07-06 10:22:21 -0700

    feat: Added request logging middleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 const routes = require("./routes");
+const logger = require("./middleware/requestLogger");
 
 const app = express();
 
 // Register middleware and API routes
 app.use(cors());
 app.use(express.json());
+app.use(logger);
 app.use("/api", routes);
 
 module.exports = app;
