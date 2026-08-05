ad7531bc17e81d71f4b46de8260c840fa2a33edf
Lan Quy Luu <luuquylan.keil@gmail.com>
2026-08-05 08:32:30 -0700

    feat: Added propertyDetailSection and refactor DescriptionSection design

--- frontend/src/components/DescriptionSection.jsx
+++ frontend/src/components/DescriptionSection.jsx
@@ -1,11 +1,11 @@
 const DescriptionSection = ({ remark }) => {
   return (
-    <section className="container">
-      <h2>About This Property</h2>
-      {remark && (
-        <div>
-          <p>{remark}</p>
-        </div>
+    <section className="mb-4">
+      <h2 className="h4 mb-3">About This Property</h2>
+      {remark ? (
+        <p style={{ whiteSpace: "pre-line" }}>{remark}</p>
+      ) : (
+        <p className="text-muted">No property description available.</p>
       )}
     </section>
   );
