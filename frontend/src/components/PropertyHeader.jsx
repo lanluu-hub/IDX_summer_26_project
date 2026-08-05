c2574a6b5a77e51f874d22dfc19bea1a6cde7cec
Lan Quy Luu <luuquylan.keil@gmail.com>
2026-08-05 08:38:12 -0700

    refactor: change PropertyHeader design

--- frontend/src/components/PropertyHeader.jsx
+++ frontend/src/components/PropertyHeader.jsx
@@ -4,17 +4,19 @@ import { formatTitle } from "../utils/formatTitle";
 
 const PropertyHeader = ({ property }) => {
   return (
-    <section className="container">
-      <span className="fs-4">{formatPrice(property.L_SystemPrice)}</span>
-      <h1>
+    <section className="mb-4">
+      <span className="display-6 fw-bold mb-1 text-success">
+        {formatPrice(property.L_SystemPrice)}
+      </span>
+      <h2 className="mb-2">
         {formatTitle(
           property.L_Type_,
           property.SubdivisionName,
           property.L_City,
           property.L_Address,
         )}
-      </h1>
-      <address className="fw-lighter fs-5">
+      </h2>
+      <div className="text-muted fs-5 mb-0">
         <i className="bi bi-geo-alt me-2"></i>
         {formatAddress(
           property.L_Address,
@@ -22,7 +24,7 @@ const PropertyHeader = ({ property }) => {
           property.L_State,
           property.L_Zip,
         )}
-      </address>
+      </div>
       <hr />
     </section>
   );
