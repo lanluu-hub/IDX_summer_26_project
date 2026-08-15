a7b84920202759574f6f2720f95e8c666e6d6663
Lan Quy Luu <luuquylan.keil@gmail.com>
2026-08-14 21:33:18 -0700

    fix: resolve nesting <a> warning

--- frontend/src/components/PropertyImageCarousel.jsx
+++ frontend/src/components/PropertyImageCarousel.jsx
@@ -9,11 +9,30 @@ const PropertyImageCarousel = ({ photos }) => {
     setActiveIdx(selectedIndex);
   };
 
+  const handleCarouselControlPrev = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    setActiveIdx((currentIdx) =>
+      currentIdx === 0 ? photos.length - 1 : currentIdx - 1,
+    );
+  };
+
+  const handleCarouselControlNext = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    setActiveIdx((currentIdx) =>
+      currentIdx === photos.length - 1 ? 0 : currentIdx + 1,
+    );
+  };
+
   return (
     <div className="position-relative">
       <Carousel
         activeIndex={activeIdx}
         onSelect={handleSelect}
+        controls={false}
         indicators={false}
         interval={null}
       >
@@ -28,6 +47,24 @@ const PropertyImageCarousel = ({ photos }) => {
           </Carousel.Item>
         ))}
       </Carousel>
+      <button
+        type="button"
+        className="carousel-control-prev"
+        onClick={handleCarouselControlPrev}
+        aria-label="Previous image"
+      >
+        <span className="carousel-control-prev-icon" aria-hidden="true" />
+      </button>
+
+      <button
+        type="button"
+        className="carousel-control-next"
+        onClick={handleCarouselControlNext}
+        aria-label="Next image"
+      >
+        <span className="carousel-control-next-icon" aria-hidden="true" />
+      </button>
+
       <span
         className="position-absolute bottom-0 end-0 m-2 badge text-bg-dark"
         style={{
